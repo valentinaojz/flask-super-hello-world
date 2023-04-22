2b861db705b713d6e0710d02d02cd1843d93408e
valentinaojz <valentina.ojeda@soymas.cl>
2023-04-22 09:55:19 -0400

    actividad 2

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-return render_template('index.html')
-@app.route('/')
-def welcome_world():
-    return render_template('welcome.html')
+    return render_template('index.html')
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html', user = request.args.get('username'))
